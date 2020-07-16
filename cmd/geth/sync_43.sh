3da5b515e7ad05f32863d6ba58f8f1559de2f8b3
yangyf <1083822363@qq.com>
2020-07-16 17:28:02 +0800

    update

--- cmd/geth/sync_43.sh
+++ cmd/geth/sync_43.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+set -ex
+
+go build --tags sm2 -a -ldflags '-extldflags "-static"' .
+
 sshpass -p "123456" scp geth eth@192.168.136.43:~/gm/
 sshpass -p "123456" scp genesis.json eth@192.168.136.43:~/gm/
